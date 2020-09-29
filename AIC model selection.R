26d839a62d0bbf4bbd922e7bad055a688af767b3
Thomas Mesaglio <48739968+tmesaglio@users.noreply.github.com>
2020-09-29 16:12:14 +1000

    Update AIC model selection.R

--- AIC model selection.R
+++ AIC model selection.R
@@ -2,18 +2,16 @@
 community <- read.csv(file = "Dataset 1 - Sydney mooring ordinal abundance data.csv", header = TRUE)
 
 #load package
-library(MASS)
+library(AICcmodavg)
 
-#run stepwise model selection process (AIC)
+#run stepwise model selection process (AICc)
 master = lm(Day~Jassa_slatteryi+Lepas_anserifera+Lepas_anatifera+Caprella_danilevskii, dat=community)
-step <- stepAIC(master, direction = "forward")
-step$anova
+AICc(master, k = 2, REML = NULL)
 master
 plot(master, which = 1)
 summary(master)
 #note that line 8 is only the final model; each species was tested individually and in combination with each other
 
-#apply AICc
-library(AICcmodavg)
-AICc(master, k = 2, REML = NULL)
+
+
 
